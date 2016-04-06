45a49818871e4fbf9af6771a415d56b9662382b4
EC2 Default User <ec2-user@ip-172-31-39-28.us-west-2.compute.internal>
2016-04-06 05:20:31 +0000

    sending to heroku

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require("express"),
     nano = require('nano')('http://localhost:5984'),
     app = express();
 
+var port = process.env.PORT || 3000 ;
+
 app.get("/", function (req, res) {
   res.send("Hey buddy!");
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Express listening on port 3000');
 });
