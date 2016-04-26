8115d76bd3deeb7654e750e29a0bdf8eba4d6de0
somenathghosh <somenath.ghosh84@gmail.com>
2016-04-25 23:14:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require("express"),
 var port = process.env.PORT || 3000 ;
 
 app.get("/", function (req, res) {
-  res.send("Hey buddy, this is CI/CD from shippable to Heroku - Again trying from RDP!");
+  res.send("Hey buddy, this is CI/CD from shippable to Heroku - Test01!");
 });
 
 app.listen(port, function () {
